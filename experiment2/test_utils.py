2b594077b81569a50e7feed8750d9da66b758b55
aris setyawan <aris@kiranatama.com>
2018-10-27 12:25:31 +0700

    exp2

---
+++ experiment2/test_utils.py
@@ -0,0 +1,16 @@
+# Mathieu Blondel, February 2012
+# License: BSD 3 clause
+
+# Port to Python of examples in chapter 5 of
+# "Introductory Statistics with R" by Peter Dalgaard
+
+from utilsClassification import *
+
+
+line()
+
+log("test")
+
+log_iter("test", 1, [[22,33],[3,2]], "halo log")
+
+log_iter("test", 3, [], "halo log")
